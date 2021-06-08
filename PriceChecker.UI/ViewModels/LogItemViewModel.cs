79d14afc16f39f3bfc5e491b69c17027042e0847
hwndmaster <5339929+hwndmaster@users.noreply.github.com>
2021-06-08 10:59:36 +0200

    Log message copy to clipboard

--- PriceChecker.UI/ViewModels/LogItemViewModel.cs
+++ PriceChecker.UI/ViewModels/LogItemViewModel.cs
@@ -1,13 +1,22 @@
 using System.ComponentModel;
+using System.Windows.Forms;
 using System.Windows.Media.Imaging;
+using Genius.PriceChecker.UI.Forms;
 using Genius.PriceChecker.UI.Forms.ViewModels;
 using Genius.PriceChecker.UI.Forms.Attributes;
 using Microsoft.Extensions.Logging;
 
 namespace Genius.PriceChecker.UI.ViewModels
 {
-  public class LogItemViewModel : ViewModelBase
+    public class LogItemViewModel : ViewModelBase
     {
+        public LogItemViewModel()
+        {
+            CopyToClipboardCommand = new ActionCommand(_ => {
+                Clipboard.SetText(Message);
+            });
+        }
+
         [IconSource(nameof(SeverityIcon), 16d)]
         public LogLevel Severity { get; set; }
         public string Logger { get; set; }
@@ -33,5 +42,8 @@ namespace Genius.PriceChecker.UI.ViewModels
 
         [Browsable(false)]
         public bool IsSeverityCritical => Severity == LogLevel.Critical;
+
+        [Icon("Copy16")]
+        public IActionCommand CopyToClipboardCommand { get; }
     }
 }
